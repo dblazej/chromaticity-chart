a3ea2cbdf98294cdcd1ce98923fbffbe2378d311
dblazej <123904190+dblazej@users.noreply.github.com>
2023-02-01 08:34:21 +0100

    final commit

---
+++ ChromaticityChart/CIEXYZ.cs
@@ -0,0 +1,15 @@
+﻿namespace Chromaticity
+{
+    public class CIEXYZ
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public CIEXYZ(double X, double Y, double Z)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.Z = Z;
+        }
+    }
+}
---
+++ ChromaticityChart/ChromaticityChart/MainWindow.cs
@@ -0,0 +1,10 @@
+namespace ChromaticityChart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
